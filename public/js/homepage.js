53c8775b97641df881581792d4aba83dd8712cac
pprachit09 <pprachit09@gmail.com>
2019-05-25 22:55:30 +0530

    adding homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,17 @@
+const content1 = () => {
+    document.getElementById("main-content").style.display = "block";
+    document.getElementById("main-content2").style.display = "none";
+    document.getElementById("main-content3").style.display = "none";
+}
+
+const content2 = () => {
+    document.getElementById("main-content").style.display = "none";
+    document.getElementById("main-content2").style.display = "block";
+    document.getElementById("main-content3").style.display = "none";
+}
+
+const content3 = () => {
+    document.getElementById("main-content").style.display = "none";
+    document.getElementById("main-content2").style.display = "none";
+    document.getElementById("main-content3").style.display = "block";
+}
