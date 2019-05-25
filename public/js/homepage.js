6cb0ac0398e3c1b284e3f93b442901746aba9743
pprachit09 <pprachit09@gmail.com>
2019-05-25 23:13:33 +0530

    adding welcome text on homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -2,16 +2,19 @@ const content1 = () => {
     document.getElementById("main-content").style.display = "block";
     document.getElementById("main-content2").style.display = "none";
     document.getElementById("main-content3").style.display = "none";
+    document.getElementById("welcome-content").style.display = "none";
 }
 
 const content2 = () => {
     document.getElementById("main-content").style.display = "none";
     document.getElementById("main-content2").style.display = "block";
     document.getElementById("main-content3").style.display = "none";
+    document.getElementById("welcome-content").style.display = "none";
 }
 
 const content3 = () => {
     document.getElementById("main-content").style.display = "none";
     document.getElementById("main-content2").style.display = "none";
     document.getElementById("main-content3").style.display = "block";
+    document.getElementById("welcome-content").style.display = "none";
 }
